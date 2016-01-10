5e16d51b57fbe8e7e17e8d1cc32954cc82bce6ef
Craig Weysham <craig@liquidcoding.biz>
2016-01-09 20:33:26 -0600

    Modified the way the current page is displayed

--- src/js/site.js
+++ src/js/site.js
@@ -1,16 +1,31 @@
-//$(document).ready(function () {
-//    $('.nav li a').click(function (e) {
-//
-//        $('.nav li').removeClass('active');
-//
-//        var $parent = $(this).parent();
-//        if (!$parent.hasClass('active')) {
-//            $parent.addClass('active');
-//        }
-//        e.preventDefault();
-//    });
-//});
+$(function () {
+
+    var url = window.location.pathname,
+        url = url.split('/').pop();
+    // now grab every link from the navigation
+    $('#myNavbar > ul > li > a').each(function () {
+        // and test its normalized href against the url pathname regexp
+        if (url == (this.href.split('/').pop())) {
+            var $parent = $(this).parent();
+            $parent.addClass('active');
+        }
+    });
+
+});
+
+$(document).ready(function () {
+    $('#brent-click').click(function (e) {
+        $(this).find('i').toggleClass('fa-angle-up').toggleClass('fa-angle-down');
+    })
+    $('#jay-click').click(function (e) {
+        $(this).find('i').toggleClass('fa-angle-up').toggleClass('fa-angle-down');
+    })
+    $('#craig-click').click(function (e) {
+        $(this).find('i').toggleClass('fa-angle-up').toggleClass('fa-angle-down');
+    })
+});
+
 $(function () {
     var theYear = new Date().getFullYear();
-    $("#copy-year").html(theYear);
+    $('#copy-year').html(theYear);
 });
